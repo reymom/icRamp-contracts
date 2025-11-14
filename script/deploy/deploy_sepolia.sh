0d1614617ba067494e138b6866c3e5419d86ab0e
reymom <ramon_gs95@hotmail.com>
2025-11-14 15:32:56 +0100

    script verify with etherscan v2

--- script/deploy/deploy_sepolia.sh
+++ script/deploy/deploy_sepolia.sh
@@ -2,4 +2,4 @@
 
 source .env
 echo "Running deploy script"
-forge script script/Deploy.s.sol:Deploy --ffi --rpc-url $SEPOLIA_RPC_URL --etherscan-api-key ${ETHERSCAN_API_KEY} --slow --broadcast --verify -vvvv --via-ir
+forge script script/Deploy.s.sol:Deploy --ffi --verifier-url "https://api.etherscan.io/v2/api?chainid=11155111" --rpc-url $SEPOLIA_RPC_URL --etherscan-api-key ${ETHERSCAN_API_KEY} --slow --broadcast --verify -vvvv
